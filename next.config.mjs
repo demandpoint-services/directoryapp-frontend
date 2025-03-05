c2608e4d21d42c1431c30e6f47e9638cb032823a
augustine-ugwu <augustinehills@yahoo.com>
2025-03-05 17:15:40 +0100

    Quick fix

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     API_BASE_URL:
       process.env.NODE_ENV === "development"
         ? "http://localhost:5000/api"
-        : "https://demandpoint-directoryapp-backend-production.up.railway.app/api",
+        : "https://directoryapp-backend.onrender.com/api",
   },
 };
 
