eb2c788c43f9144aed98e4332580bcf4653a6212
augustine-ugwu <augustinehills@yahoo.com>
2025-03-28 19:48:01 +0100

    Quick fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+  env: {
+    API_BASE_URL:
+      process.env.NODE_ENV === "development"
+        ? "http://localhost:5000/api"
+        : "https://directoryapp-backend-production.up.railway.app/api",
+  },
 };
 
 export default nextConfig;
