644fe4d0483d709154406990300a1b7409840226
augustine-ugwu <augustinehills@yahoo.com>
2025-04-10 15:18:40 +0100

    Fixed admin auth

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -14,7 +14,7 @@ import Analytics from "../../components/Analytics";
 import Clients from "../../components/Clients";
 import Tasks from "../../components/Tasks";
 import AppTheme from "../shared-theme/AppTheme";
-// import AuthGuard from "../../components/AuthGuard";
+import AuthGuard from "../../components/AuthGuard";
 
 import {
   chartsCustomizations,
@@ -51,34 +51,34 @@ export default function AdminPage(props) {
   };
 
   return (
-    // <AuthGuard>
-    <AppTheme {...props} themeComponents={xThemeComponents}>
-      <CssBaseline enableColorScheme />
-      <Box sx={{ display: "flex" }}>
-        <SideMenu onMenuClick={handleMenuClick} />
-        <AppNavbar onMenuClick={handleMenuClick} />
-        <Box
-          component="main"
-          sx={(theme) => ({
-            flexGrow: 1,
-            backgroundColor: theme.vars
-              ? `rgba(${theme.vars.palette.background.defaultChannel} / 1)`
-              : alpha(theme.palette.background.default, 1),
-            overflow: "auto",
-          })}>
-          <Stack
-            spacing={2}
-            sx={{
-              alignItems: "center",
-              mx: 3,
-              pb: 5,
-              mt: { xs: 8, md: 0 },
-            }}>
-            {renderContent()}
-          </Stack>
+    <AuthGuard>
+      <AppTheme {...props} themeComponents={xThemeComponents}>
+        <CssBaseline enableColorScheme />
+        <Box sx={{ display: "flex" }}>
+          <SideMenu onMenuClick={handleMenuClick} />
+          <AppNavbar onMenuClick={handleMenuClick} />
+          <Box
+            component="main"
+            sx={(theme) => ({
+              flexGrow: 1,
+              backgroundColor: theme.vars
+                ? `rgba(${theme.vars.palette.background.defaultChannel} / 1)`
+                : alpha(theme.palette.background.default, 1),
+              overflow: "auto",
+            })}>
+            <Stack
+              spacing={2}
+              sx={{
+                alignItems: "center",
+                mx: 3,
+                pb: 5,
+                mt: { xs: 8, md: 0 },
+              }}>
+              {renderContent()}
+            </Stack>
+          </Box>
         </Box>
-      </Box>
-    </AppTheme>
-    // </AuthGuard>
+      </AppTheme>
+    </AuthGuard>
   );
 }
